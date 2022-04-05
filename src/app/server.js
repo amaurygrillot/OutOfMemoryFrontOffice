ad5e52481bf5ffec4ed5415a25083ddb682815b2
amaury <grillotamaury@gmail.com>
2022-04-05 10:38:21 +0200

    add server.js

---
+++ src/app/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static('./dist/PokemonArena'));
+app.get('/', function (req, res) {
+  res.sendFile('index.html', {root : 'dist/out-of-memory-front-office'});
+});
+app.listen(process.env.PORT || 8080);
