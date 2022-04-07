46e7244153d37ba1c5a05612a0c8cbf799fc8ddc
amaury <grillotamaury@gmail.com>
2022-04-07 16:08:47 +0200

    change url

--- src/app/code-editor/full-code-editor.component.ts
+++ src/app/code-editor/full-code-editor.component.ts
@@ -70,7 +70,7 @@ export class FullCodeEditorComponent implements OnInit {
     const formData: FormData = new FormData();
     formData.append('fileKey', file, programmingLanguage?.mainFile);
     console.log(programmingLanguage);
-    const data = await lastValueFrom(this.http.post<string>(`https://outofmemoryerror-code-executor.azurewebsites.net/${programmingLanguage?.languageName}/`, formData));
+    const data = await lastValueFrom(this.http.post<string>(`https://oome-code-executer.herokuapp.com/${programmingLanguage?.languageName}/`, formData));
     console.log(data);
     if(data.search('Process ended with error code : 0') === -1)
     {
